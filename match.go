9198d95ad032a3b76a4cdf8ec7dec4e8f62da0c7
c1982 <aspsrc@gmail.com>
2018-06-18 18:28:15 +0300

    yes yes something like that

--- match.go
+++ match.go
@@ -1,85 +1,111 @@
 package matching
 
 import (
-	"sync"
-	"time"
+	"container/heap"
+	"fmt"
+
+	"github.com/shopspring/decimal"
+)
+
+type OrderSide int
+type OrderType int
+type OrderStatus int
+
+const (
+	SideBUY OrderSide = iota + 1
+	SideSELL
+
+	OrderTypeMARKET = iota + 1
+	OrderTypeLIMIT
+	OrderTypeSTOP
+	OrderTypeCANCEL
+
+	OrderStatusNew OrderStatus = iota + 1
+	OrderStatusPartiallyFilled
+	OrderStatusFilled
+	OrderStatusCancelled
+	OrderStatusRejected
+	OrderStatusPending
 )
 
 type Order struct {
-	Side   string
-	Status string
-	Stop   float64
-	Amont  float64
-	Price  float64
+	ID          int
+	BaseAsset   string
+	SecondAsset string
+	Status      OrderStatus
+	Side        OrderSide
+	Type        OrderType
+	Price       decimal.Decimal
+	Amount      decimal.Decimal
 }
 
-type OrderList struct {
-	sync.RWMutex
-	Orders []Order
+func (o *Order) ToPair() string {
+	return fmt.Sprintf("%s/%s", o.BaseAsset, o.SecondAsset)
 }
 
-func (ol *OrderList) Append(item Order) {
-	ol.Lock()
-	defer ol.Unlock()
-
-	ol.Orders = append(ol.Orders, item)
+type Level struct {
+	Price decimal.Decimal `json:"price"`
+	Order []*Order        `json:"orders"`
 }
 
-func (ol *OrderList) Item() <-chan Order {
-	c := make(chan Order)
+type LevelHeap []Level
 
-	f := func() {
-		ol.Lock()
-		defer ol.Unlock()
-		for _, v := range ol.Orders {
-			c <- v
+func (h LevelHeap) Len() int           { return len(h) }
+func (h LevelHeap) Less(i, j int) bool { return h[i].Price.LessThan(h[j].Price) }
+func (h LevelHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *LevelHeap) Push(x interface{}) {
+	for index, level := range *h {
+		if x.(Level).Price.Equal(level.Price) {
+			(*h)[index].Order = append((*h)[index].Order, x.(Level).Order...)
+			return
 		}
-		close(c)
 	}
-	go f()
-
-	return c
+	*h = append(*h, x.(Level))
 }
 
-func (ol *OrderList) First() Order {
-	return ol.Orders[0]
+func (h *LevelHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
 
-var Takers *OrderList
-var Makers *OrderList
-
-func Run() {
-
-	for {
-
-		time.Sleep(time.Second * 3)
+func (h *LevelHeap) Peek() *Level {
+	if h.Len() == 0 {
+		return nil
 	}
+	return &(*h)[0]
 }
 
-func Match(takers *OrderList, makers *OrderList) {
-
-	for {
-		tk := Takers.First()
-		completed, fee := Collect(tk)
-	}
-}
-
-func Collect(o Order) (completed bool, fee float64) {
-
-	expect := o.Amont
-	filled := []Order{}
-
-	for _, v := range Makers.Orders {
-		if v.Amont == expect {
-			v.Status = "completed"
-			completed = true
-			break
-		}
-
-		if v.Amont < expect {
-			filled = append(filled, v)
+func (h *LevelHeap) Remove(o Order) error {
+	for index, level := range *h {
+		if o.Price == level.Price {
+			for i, order := range level.Order {
+				if o.ID == order.ID {
+					(*h)[index].Order = append((*h)[index].Order[:i], (*h)[index].Order[i+1:]...)
+					if len((*h)[index].Order) == 0 {
+						heap.Remove(h, index)
+					}
+					return nil
+				}
+			}
 		}
 	}
 
-	return
+	return fmt.Errorf("failed")
+}
+
+func NewTrade(o Order) {
+
+	switch o.Side {
+	case SideBUY:
+		//Bir şey
+	case SideSELL:
+	//Bir şeuy
+	default:
+		//Rejected
+
+	}
 }
